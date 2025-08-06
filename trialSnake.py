fbc7853d41f38f74d7cd979bf639f4a5acacba05
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-06 14:56:55 +0100

    Create trialSnake.py

---
+++ trialSnake.py
@@ -0,0 +1,24 @@
+class NotAStringError(Exception):
+  """ Raised if the parameter is not a string"""
+  pass
+  
+
+def testing(parameter):
+  """
+  Description goes here
+
+  :param parameter: Non-optional string to be passed in
+  :type parameter: string
+  :raise: trialSnake.NotAStringError: If the parameter is not a string.
+  :return: The string backwards
+  :rtype: string
+  """
+  try:
+    if type(parameter) != str:
+      raise NotAStringError
+
+  except NotAStringError:
+    print("Not a string")
+    return
+
+  return parameter[::-1]
