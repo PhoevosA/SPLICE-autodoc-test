c4e042f12a6011cf77083c3925fa7f0649cea68b
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-06 16:45:46 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 import os 
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'SPLICE test'
 copyright = '2025, Me'
