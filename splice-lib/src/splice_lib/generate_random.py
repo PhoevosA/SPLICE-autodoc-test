6d6cbd2374338d7c11756ba95648560ed143ffed
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-07 13:17:31 +0100

    Create generate_random.py

---
+++ splice-lib/src/splice_lib/generate_random.py
@@ -0,0 +1,10 @@
+import uuid
+def random_number() -> int:
+    """
+    This function generates a random UUID code.
+
+    :param None:
+    :return: uuid.
+    :rtype: int
+    """
+    return uuid.uuid4()
