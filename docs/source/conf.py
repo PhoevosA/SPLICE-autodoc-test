a086fcf95d79f6e342b53a51c10409164a91d831
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-07 09:51:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 import os 
 import sys
-sys.path.insert(0, str(Path('..', 'testFolder/src/spliceFolder').resolve()))
+sys.path.insert(0, os.path.abspath('testFolder/src/spliceFolder'))
 
 project = 'SPLICE test'
 copyright = '2025, Me'
