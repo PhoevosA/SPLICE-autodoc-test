8e2007222da062a2243e6a6c9d3ec0edee784ceb
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-07 09:47:59 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@
 # -- Project information
 import os 
 import sys
-sys.path.insert(0, os.path.abspath('testFolder/src/spliceFolder'))
 sys.path.insert(0, str(Path('..', 'testFolder/src/spliceFolder').resolve()))
 
 project = 'SPLICE test'
