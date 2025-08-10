83b4547c5c6df2b0ab3f7bc89188e51e9ea69493
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-11 00:44:35 +0100

    Update generate_rst.py

--- generate_rst.py
+++ generate_rst.py
@@ -6,16 +6,17 @@ for path in folder_paths:
   python_files = [f for f in os.listdir(path) if f.endswith('.py')]
   page_name = path.split("/")[0]
   rst_filename = f"{page_name}.rst"
+  with open(os.path.join("docs/source", rst_filename), 'w') as f:
   
-  for python_file in python_files:
-      module_name = os.path.splitext(python_file)[0]
+  
+    f.write(f"{page_name}\n")
+    f.write("=" * len(page_name) + "\n")
+    for python_file in python_files:
+        module_name = os.path.splitext(python_file)[0]
       
-      with open(os.path.join("docs/source", rst_filename), 'w') as f:
-          f.write(f"{page_name}\n")
-          f.write("=" * len(page_name) + "\n")
-          f.write(f"{module_name}\n")
-          f.write("\n")
-          f.write(f".. automodule:: {module_name}\n")
-          f.write("   :members:\n")
-          f.write("   :undoc-members:\n")
-          f.write("   :show-inheritance:\n")
+        f.write(f"{module_name}\n")
+        f.write("\n")
+        f.write(f".. automodule:: {module_name}\n")
+        f.write("   :members:\n")
+        f.write("   :undoc-members:\n")
+        f.write("   :show-inheritance:\n")
