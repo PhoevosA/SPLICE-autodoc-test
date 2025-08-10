0695f705f67146d929fd3b2b0ad95fb7b013a846
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-10 18:34:46 +0100

    Update generate_rst.py

--- generate_rst.py
+++ generate_rst.py
@@ -11,6 +11,7 @@ for path in folder_paths:
       module_name = os.path.splitext(python_file)[0]
       
       with open(os.path.join("docs/source", rst_filename), 'w') as f:
+          f.write(f"page_name")
           f.write(f"{module_name}\n")
           f.write("=" * len(module_name) + "\n")
           f.write("\n")
