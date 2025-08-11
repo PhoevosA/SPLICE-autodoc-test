d1598b286b369553c4cf3840dda0969147a60ef5
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-11 14:59:42 +0100

    Update test_generate.py

--- test_generate.py
+++ test_generate.py
@@ -3,12 +3,11 @@ import os
 folder_paths = ['splice-api/app','splice-cli/src/splice_cli','splice-lib/src/splice_lib']
 
 with open("docs/source/Summary.rst", 'w') as f:
+  f.write("Summary" + "\n")
+  f.write("=======" + "\n")
   for path in folder_paths:
     python_files = [file for file in os.listdir(path) if file.endswith('.py') and file != "__init__.py"]
     
-  
-    f.write("Summary" + "\n")
-    f.write("=======" + "\n")
     for python_file in python_files:
         module_name = python_file.split(".")[0]
         f.write(".. autosummary::\n")
