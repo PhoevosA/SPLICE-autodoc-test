0b66814d13b062c0824a1bc258f5b1dc79c95c9d
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-11 14:55:55 +0100

    Update test_generate.py

--- test_generate.py
+++ test_generate.py
@@ -2,10 +2,11 @@ import os
 
 folder_paths = ['splice-api/app','splice-cli/src/splice_cli','splice-lib/src/splice_lib']
 
-for path in folder_paths:
-  python_files = [f for f in os.listdir(path) if f.endswith('.py') and f != "__init__.py"]
+with open("Summary.rst", 'w') as f:
+  for path in folder_paths:
+    python_files = [file for file in os.listdir(path) if file.endswith('.py') and file != "__init__.py"]
+    
   
-  with open("Summary.rst", 'w') as f:
     f.write("Summary\n")
     f.write("=======\n")
     for python_file in python_files:
