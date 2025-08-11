c5582a95403c5ec1125aa293affacaa7e0ca42fb
PhoevosA <3015850A@student.gla.ac.uk>
2025-08-11 16:35:44 +0100

    Update and rename appropriateName.py to example_file.py

---
+++ splice-cli/src/splice_cli/example_file.py
@@ -0,0 +1,34 @@
+__doc__ = """This should describe the purpose of this file"""
+
+class ExampleError(Exception):
+    """ Raised if the parameter is not a string"""
+    pass
+
+
+def exampleFunction(parameter):
+    """
+    
+	Description of this function
+    
+	Extended descriptions will appear on automodules but not on the autosummaries (i.e. on the individual pages vs on the summaries page).
+	This text can be as long as you like and you can put a blank line between paragraphs in your description
+
+	like so, to format them properly
+  
+	:param example: Word to be passed into exampleFunction
+	:type example: string, not optional 
+	:raise example_file.ExampleError: If the parameter is not a string 
+	:return: The string backwards 
+	:rtype: string 
+	"""
+    try:
+        if type(parameter) != str:
+            raise NotAStringError
+
+    except NotAStringError:
+        print("Not a string")
+        return
+
+    return parameter[::-1]
+
+print(exampleFunction("This is an example string"))
